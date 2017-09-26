052bc0e008ea8d90d571ff12545b52c07242639b
chrilleson <Christoffer.Ljungqvist@hotmail.se>
2017-09-26 13:20:20 +0200

    Ytterväggarna är skapade och finns i Wall.cs

--- Labb4Game/Map.cs
+++ Labb4Game/Map.cs
@@ -9,19 +9,19 @@ namespace Labb4Game
 {
     class Map
     {
+        public string[,] table = new string[6, 14];
         public Map()
         {
-            int[,] table = new int[5, 6];
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 6; i++)
             {
-                for (int j = 0; j < 6; j++)
+                for (int j = 0; j < 14; j++)
                 {
-                    table[i, j] = i * j;
+                    table[i, j] = ".";
                 }
             }
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 6; i++)
             {
-                for (int j = 0; j < 6; j++)
+                for (int j = 0; j < 14; j++)
                 {
                     Console.Write(table[i, j]);
                 }
--- Labb4Game/Wall.cs
+++ Labb4Game/Wall.cs
@@ -8,6 +8,37 @@ namespace Labb4Game
 {
     class Wall : Map
     {
+        public Wall()
+        {
 
+            //Skapar ytterväggarna
+            for (int i = 0; i < 6; i++)
+            {
+                for(int j = 0; j < 14; j++)
+                {
+                    if (j == 0 || j == 13)
+                        table[i, j] = "#";
+                }
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 14; j++)
+                {
+                    if(i == 0 || i == 5)
+                        table[i, j] = "#";
+                }
+            }
+
+
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 14; j++)
+                {
+                    Console.Write(table[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
     }
 }
